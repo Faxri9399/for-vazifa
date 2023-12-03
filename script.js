24042930311855bff7bbf008c15cefca63495b47
TEXNO <fbbozorov@gmail.com>
2023-12-03 22:17:05 +0500

    for-vazifa

---
+++ script.js
@@ -0,0 +1,134 @@
+//for-1
+/*let k = 10;
+let n = 3;
+for(i=0; i<n; i++) {
+  console.log(k);
+}*/
+//for-2
+/*let a = 2;
+let b = 5;
+for(let i=2; i<=b; i++) {
+  console.log(i);
+}*/
+//for-3
+/*let a = 3;
+let b = 7;
+for(let i=6; i>a; i--) {
+  console.log(i); 
+}*/
+//for-4
+/*let m = 50000;
+for(let i=1; i<10; i++) {
+  console.log(i*m);
+}*/
+//for-5
+/*let m = 35000
+for(let i=0.1; i<1; i+=0.1) {
+  console.log(i.toFixed(1)*m);
+}*/
+//for-6
+/*let m = 25000
+for(let i=1.2; i<2; i+=0.2) {
+  console.log(i.toFixed(1)*m);
+}*/
+//for-7
+/*let a = 3;
+let b = 7;
+let s = 0;
+for(let i=3; i<=b; i++) {
+  s+=i
+} console.log(s);*/
+//for-8
+/*let a = 3;
+let b = 5;
+let s = 1;
+for(let i=3; i<=b; i++) {
+  s*=i
+} console.log(s);*/
+//for-9
+/*let a = 2;
+let b =  4;
+let s = 0;
+for(let i=2; i<=b; i++) {
+  s+=i**2
+}console.log(s);*/
+//for-10
+/*let n = 3;
+let s = 0;
+for(let i=1; i<=n; i++) {
+  s+=1/i
+}console.log(s);*/
+//for-11
+/*let n =5;
+let s = 0;
+for(let i=1; i<=n; i++) {
+  s+=i**2
+}console.log(s);*/
+//for-12
+/*let n = 2;
+let s = 1;
+for(let i=1; i<=n; i+=0.1) {
+  s*=i
+}console.log(s);*/
+//for-13
+/*let n = 1.5;
+let s = 0;
+for(let i=1; i<=n; i-=-0.1) {
+  s+=-i
+}console.log(s);*/
+//for-14
+/*let n = 15;
+let s = 0;
+for(let i=1; i<n; i+=2) {
+  s+=i**2;
+  console.log(i);
+}console.log(s);*/
+//for-15
+/*let n = 2;
+let a = 3;
+let s;
+for(let i=a; i<=a; i++) {
+  s=Math.pow(a,n)
+}console.log(s);*/
+//for-16
+/*let n = 4;
+let a = 2;
+let k;
+for(let i=1; i<=n; i++) {
+  for(let j=1; j<=i; j++) {
+    k=Math.pow(a,n)
+  }
+  console.log(k);
+}*/
+
+//for-19
+/*let n = 3;
+for(let i=1; i<=n; i++) {
+  let k=1;
+  for(let j=1; j<=i; j++){
+    k*=j;
+  }
+    console.log(k);
+}*/
+//for-20
+/*let n = 3;
+let s = 0;
+for(let i=1; i<=n; i++) {
+  let k=1;
+  for(let j=1; j<=i; j++){
+    k*=j;
+  }
+    s+=k;
+}
+console.log(s);*/
+//for-21
+/*let n = 3;
+let s = 0;
+for(let i=1; i<=n; i++) {
+  let k=1;
+  for(let j=1; j<=i; j++){
+    k*=j;
+  }
+    s+=1/k;
+}
+console.log(s);*/
